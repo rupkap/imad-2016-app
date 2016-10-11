d0bfa421fb98037b21c8605fffadea0e119046f4
rupkap <rupkap15@gmail.com>
2016-10-11 17:55:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,13 @@ console.log('Loaded!');
 //change the etxt
 var element = document.getElementById('main-text');
 element.innerHTML='new stuff';
+var img= documnet.getElementById(madi);
+var m=0;
+function moveRight(){
+    m=m+1;
+    img.style.m=m+'px';
+    
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+};
